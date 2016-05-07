0b3056a24ed9f1fce48aa6c7b56d5a540a24b34c
Beto23 <josee-45@hotmail.com>
2016-05-07 11:29:57 -0500

    Add model HasCategories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507160147) do
+ActiveRecord::Schema.define(version: 20160507162810) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "titulo"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20160507160147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "has_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "has_categories", ["article_id"], name: "index_has_categories_on_article_id"
+  add_index "has_categories", ["category_id"], name: "index_has_categories_on_category_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
