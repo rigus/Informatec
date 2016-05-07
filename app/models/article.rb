066322b640553a66de5c4c1ffe31077025c8ffd9
Beto23 <josee-45@hotmail.com>
2016-05-07 15:30:27 -0500

    Add tables relationship: categories and has_categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,13 @@
 class Article < ActiveRecord::Base
-  attr_reader :categories
+  has_many :has_categories
+  has_many :categories, through: :has_categories
   after_create :save_categories
   has_attached_file :cover, styles: {medium: "1280x720", thumb: "800x600"}
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 
   #Custom setter
-  def categories=(categories)
-    @categories = categories
+  def categories=(value)
+    @categories = value
   end
 
   private
