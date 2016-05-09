6bf933411fd3ec7fe66c3f172349c70905d1b21c
Beto23 <josee-45@hotmail.com>
2016-05-08 21:46:06 -0500

    Add design for categories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,3 +42,8 @@ $('.dropdown-button').dropdown({
     alignment: 'left' // Displays dropdown with edge aligned to the left of button
   }
 );
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
